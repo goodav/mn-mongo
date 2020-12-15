7572002a2aea169484615f846043582c1d8e8baf
David Goodin <dgoodin@Davids-MacBook-Pro.local>
2020-12-15 08:16:51 -0600

    save

--- src/main/kotlin/com/example/app/service/OrderService.kt
+++ src/main/kotlin/com/example/app/service/OrderService.kt
@@ -4,38 +4,41 @@ import com.example.app.domain.Item
 import com.example.app.domain.Order
 import com.example.app.domain.OrderType
 import com.example.app.repo.MongoRepository
-import kotlinx.coroutines.CoroutineScope
 import java.util.*
 import javax.inject.Singleton
-import kotlin.coroutines.CoroutineContext
 
 @Singleton
 class OrderService(
-  val mongoRepository: MongoRepository//,
-  //override val coroutineContext: CoroutineContext
-) {//: CoroutineScope {
-  suspend fun getOrderById(orderId: Long) : Order? {
-    create()
-    val order = mongoRepository.findOrderById(orderId)
-    return order
-  }
+    val mongoRepository: MongoRepository//,
+    //override val coroutineContext: CoroutineContext
+) {
+    //: CoroutineScope {
+    suspend fun getOrderById(orderId: Long): Order? {
+        create()
+        val order = mongoRepository.findOrderById(orderId)
+        return order
+    }
 
-  suspend fun create() {
-    var order = Order(
-      orderId = System.currentTimeMillis(),
-      firstName = "Hootie",
-      lastName = "Blowfish",
-      orderType = OrderType.Pickup("pickup"),
-      createdDate = Date(),
-      completedTimestamp = null,
-      items = mutableListOf(
-        Item(
-          itemId = System.currentTimeMillis(),
-          description = "foo",
-          qty = 1
+    suspend fun create() {
+        var order = Order(
+            orderId = System.currentTimeMillis(),
+            firstName = "Hootie",
+            lastName = "Blowfish",
+            orderType = OrderType.Pickup("pickup"),
+            createdDate = Date(),
+            completedTimestamp = null,
+            items = mutableListOf(
+                Item(
+                    itemId = System.currentTimeMillis(),
+                    description = "foo",
+                    qty = 1
+                )
+            )
         )
-      )
-    )
-    mongoRepository.save(order)
-  }
+        mongoRepository.save(order)
+    }
+
+//  suspend fun foo() {
+//    mongoRepository.mongoCollection
+//  }
 }
